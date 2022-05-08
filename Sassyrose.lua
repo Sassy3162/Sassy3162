e75421de23ed403857aea3049818bcf0cae436f0
Sassy3162 <103266590+Sassy3162@users.noreply.github.com>
2022-05-08 13:08:54 +0300

    Sassyrose.lua

--- Sassyrose.lua
+++ Sassyrose.lua
@@ -1,3 +1,1 @@
 gg.alert("💙Güncelleniyor...")
-gg.alert("Otomatik olarak güncellenir. Güncellemeleri script kapatıp açınca gelmektedir. Elinde bu dosya olanlar tekrar indirmesine gerek yoktur‼️
--Sassy")
