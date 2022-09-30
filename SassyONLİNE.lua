c9420547ace3d680f231df8f74b3818db09c3975
Sassy3162 <103266590+Sassy3162@users.noreply.github.com>
2022-09-30 17:12:45 +0300

    SassyONLİNE.lua

--- SassyONLİNE.lua
+++ SassyONLİNE.lua
@@ -1,1 +1,251 @@
+Password = 'SWHBj6e7kJqkMSk'
+PS = gg.prompt({'🔒 KEYİ GİRİNİZ: '},{[1]=''},{[1]='number'})
+if not PS then os.exit() 
+end 
+if PS[1] == Password then 
+gg.toast('✔️ GİRİŞ BAŞARILI')
+else 
+gg.alert('❌ GİRİŞ BAŞARISIZ ❕') os.exit() end
+
+
+
+
+
+
+
+
+
+
+
+
+gg.alert("Sassy vip gg💎")
+gg.alert("2.2~All~ versiyon")
+
+Date = "20220927"
+date = os.date("%Y%m%d")
+if date >= Date then
+  print([[SÜRESİ BİTTİ DATE EXPRİED😭]])
+  return
+  end
+
+
+
+
+function setvalue(address,flags,value)  local refinevalues={}  refinevalues[1]={}  refinevalues[1].address=address  refinevalues[1].flags=flags  refinevalues[1].value=value  gg.setValues(refinevalues)  end 
+
+
+
+
+on = '[❌]'
+off = '[✅]'
+
+
+
+
+
+
+
+
+
+
+
+function HOME()
+menu1 = gg.choice({
+"𝙇𝙊𝙂𝙊 𝙆𝙊𝙍𝙐𝙔𝙐𝘾𝙐",
+"𝙇𝙊𝘽𝘽𝙔 𝙆𝙊𝙍𝙐𝙔𝙐𝘾𝙐",
+"𝙍𝙀𝙋𝙊𝙍𝙏𝙇𝘼𝙍𝙄 𝙔𝙊𝙆 𝙀𝙏",
+"𝙊𝙔𝙐𝙉 𝙏𝙀𝙆𝙍𝘼𝙍𝙄 𝙆𝘼𝙋𝘼𝙇𝙄",
+"❌",
+}, nil," 𝙎𝘼𝙎𝙎𝙔 𝙁𝙐𝙇𝙇 𝙆𝙊𝙍𝙐𝙈𝘼💎   ")
+
+if menu1 == nil then else
+if menu1== 1 then lg() end
+if menu1== 2 then by() end
+if menu1== 3 then sekme() end
+if menu1 == 4 then aimbot() end
+if menu1 == 5 then ex()end
+
+
+
+end
+ iBug = -1
+ end
+
+
+
+function lg()
+so=gg.getRangesList('libanogs.so')[1].start
+setvalue(so + 0x3432A,4,781)
+setvalue(so + 0x33F9A,4,781)
+setvalue(so + 0x34816,4,781)
+setvalue(so + 0x3484E,4,781)
+setvalue(so + "0x33F9A",4,"h0D 03")
+setvalue(so + "0x34816",4,"h0D 03")
+setvalue(so + "0x3484E",4,"h0D 03")
+setvalue(so + "0x34350", 4, "h0D 03")
+local anogs = gg.getRangesList("libanogs.so")[1].start 
+ setvalue(anogs + "0x1997b4", 4, "h 59 00")
+ setvalue(anogs + "0x262c76", 4, "h 00 00 00 00 ")
+ setvalue(anogs + "0x43206", 4, "h 00 20 70 47")
+setvalue(anogs + "0x31370", 32, "h 00 00 B0 E3 1E FF 2F E1")
+setvalue(anogs + "0x93870", 32, "h 00 00 B0 E3 1E FF 2F E1")
+setvalue(anogs + "0x31a650", 32, "h 1E FF 2F E1")
+setvalue(anogs + "0x31a64c", 32, "h 00 0F 02 E0")
+setvalue(so + "0x107500", 32, "h 00 00 B0 E3 1E FF 2F E1")
+setvalue(so + "0x107514", 32, "h 00 00 B0 E3 1E FF 2F E1")
+gg.alert("💎😉")
+
+
+end
+
+function by()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("620137442967552;564058054983680",gg.TYPE_QWORD)
+gg.refineNumber("620137442967552",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("635530605756416;564058054983680",gg.TYPE_QWORD)
+gg.refineNumber("635530605756416",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("582749752655872;564058054983680",gg.TYPE_QWORD)
+gg.refineNumber("582749752655872",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("578351706144768;564058054983680",gg.TYPE_QWORD)
+gg.refineNumber("578351706144768",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("577252194516992;288233678981562368",gg.TYPE_QWORD)
+gg.refineNumber("577252194516992",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("303473799200768",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("579451217772544",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("914737839734784",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("131330;134658",gg.TYPE_DWORD)
+gg.refineNumber("131330",gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("67109633",gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("134658;131330",gg.TYPE_DWORD)
+gg.refineNumber("134658",gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("67109633",gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("565157566611456",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("566257078239232",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("578351706144768",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("565157566611456",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.searchNumber("577252194516992",gg.TYPE_QWORD)
+gg.getResults(50000)
+gg.editAll("288233678981562368",gg.TYPE_QWORD)
+gg.alert("💎😉")
+end
+
+
+
+
+function sekme()
+gg.setVisible(false)
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber(":tss_sdk_rcv_anti_data", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber(":tss_sdk_rcv_anti_data", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll(":0", gg.TYPE_BYTE)
+gg.clearResults()
+gg.searchNumber(":TssSDKInit", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber(":TssSDKInit", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll(":0", gg.TYPE_BYTE)
+gg.clearResults()
+gg.searchNumber(":tss_del_report_data", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber(":tss_del_report_data", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll(":0", gg.TYPE_BYTE)
+gg.clearResults()
+gg.searchNumber(":tss", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber(":tss", gg.TYPE_BYTE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(5000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.editAll(":0", gg.TYPE_BYTE)
+gg.clearResults()
+gg.alert("💎😉")
+end
+
+function aimbot ()
+gg.clearResults()
+gg.setRanges(4)
+gg.searchNumber("909391408;808923191::8", 4)
+gg.getResults(999)
+gg.editAll("1089886885", 4)
+gg.clearResults()
+gg.setRanges(4)
+gg.searchNumber("909391408", 4)
+gg.getResults(999)
+gg.editAll("1089886885", 4)
+gg.clearResults()
+gg.alert("💎😉")
+end
+
+
+
+function ex()
+print("❌👋")
+gg.skipRestoreState()
+os.exit()
+end
+
+while true do
+  if gg.isVisible(true) then
+    iBug = 1
+    gg.setVisible(false)
+  end
+  gg.clearResults()
+  if iBug == 1 then
+    HOME()
+  end  
+end
+
 
